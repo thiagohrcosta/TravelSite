6f610ad245799cf99c9d7b3a4c62fcfa1007c937
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-09-07 20:29:23 -0300

    Heroku fix bug

--- index.js
+++ index.js
@@ -80,9 +80,8 @@ app.get("/posts/:postId", function (req, res) {
 let port = process.env.PORT;
 if (port == null || port == ""){
   port = 3000;
-};
-
+}
 
 app.listen(port, function(){
   console.log("Server has started successfully.")
-});
+})
