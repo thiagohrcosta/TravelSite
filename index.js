5db6600b80a51bc9095f67c7486b3391dffac165
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-09-07 20:14:06 -0300

    Heroku deploy

--- index.js
+++ index.js
@@ -76,12 +76,13 @@ app.get("/posts/:postId", function (req, res) {
 });
 
 
-/*
+
 let port = process.env.PORT;
 if (port == null || port == ""){
   port = 3000;
-}
-*/
-app.listen(3000, function(){
+};
+
+
+app.listen(port, function(){
   console.log("Server has started successfully.")
-})
+});
